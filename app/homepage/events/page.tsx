cb3be6307306c0012c5a7bf0039c604c131bd0d3
Vishwesh Aryan <148064350+Int-vishwesh@users.noreply.github.com>
2025-08-30 16:59:01 +0530

    homepage and nested ui

---
+++ app/homepage/events/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+export default function Events(){
+  return(
+    <>
+    <div>
+      <p>all/live jb</p>
+      <Image src={"/landing.jpg"} alt="event 1" width={500} height={500}/>
+    </div>
+    </>
+  )
+}
